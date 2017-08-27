0aa7064b4f6265805b873f7e4e260ac3d3dfed27
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2017-08-28 03:43:24 +0900

    :sparkles: Upgrade to Bootstrap 4 and use bootstrap-vue.

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,22 @@
+import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import './sass/main.scss';
 
 import { HomeComponent } from './components/home';
-import { AboutComponent } from './components/about';
-import { ListComponent } from './components/list';
-import { NavbarComponent } from './components/navbar';
 
 // register the plugin
+Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 
 let router = new VueRouter({
   routes: [
-    { path: '/', component: HomeComponent },
-    { path: '/about', component: AboutComponent },
-    { path: '/list', component: ListComponent },
+    { path: '/', component: HomeComponent }
   ]
 });
 
 new Vue({
   el: '#app-main',
-  router: router,
-  components: {
-    'navbar': NavbarComponent
-  }
+  router: router
 });
