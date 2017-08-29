c8d75c9a897e464fa624734bfdc24862463edc96
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2017-08-29 13:57:33 +0900

    :construction: Stacked icon

--- src/components/icon-label/icon-label.ts
+++ src/components/icon-label/icon-label.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
+import './icon-label.scss';
+
 class IconLabelParrams {
-  icon: string;
+  icon: string | Array<string>;
+  extra: string | Array<string>;
   label: string;
   __raw: string;
 }
@@ -20,13 +23,47 @@ export class IconLabelComponent extends Vue {
   // data
   public label: string = this.params.label || '';
   public raw: string = this.params.__raw || '';
+  public stacked: boolean = !(typeof this.params.icon === 'string');
 
   // msg
   get icon(): string {
-    return `fa fa-lg fa-fw fa-${this.params.icon}`;
+    return this.getIconClass(0);
+  }
+
+  get icon1(): string {
+    return this.getIconClass(0);
+  }
+
+  get icon2(): string {
+    return this.getIconClass(1);
   }
 
   get hasLabel(): boolean {
     return this.label !== '';
+  }
+
+  private getIconClass(index: number) {
+    let line = `fa fa-${this.getValue(this.params.icon, index)}`;
+    if (this.getValue(this.params.extra, index) !== '') {
+      line += ` ${this.getValue(this.params.extra, index)}`;
+    }
+    if (this.stacked) {
+      let stackIndex = index === 0 ? 1 : 2;
+      line += ` fa-stack-${stackIndex}x`;
+    } else {
+      line += ' fa-fw fa-lg';
+    }
+    return line;
+  }
+
+  private getValue(obj: any, index: number) {
+    if (obj === null) {
+      return '';
+    }
+    if (this.stacked) {
+      return obj[index] || '';
+    } else {
+      return obj || '';
+    }
   }
 }
--- src/components/linked-icon-label/linked-icon-label.ts
+++ src/components/linked-icon-label/linked-icon-label.ts
@@ -7,7 +7,8 @@ class LinkedIconLabelLinkParams {
 }
 
 class LinkedIconLabelParrams {
-  icon: string;
+  icon: string | Array<string>;
+  extra: string | Array<string>;
   label: string;
   __raw: string;
   link: LinkedIconLabelLinkParams;
@@ -27,13 +28,47 @@ export class LinkedIconLabelComponent extends Vue {
   public label: string = this.params.label || '';
   public raw: string = this.params.__raw || '';
   public link: LinkedIconLabelLinkParams = this.params.link || { text: '', href: '#' };
+  public stacked: boolean = !(typeof this.params.icon === 'string');
 
   // msg
   get icon(): string {
-    return `fa fa-lg fa-fw fa-${this.params.icon}`;
+    return this.getIconClass(0);
+  }
+
+  get icon1(): string {
+    return this.getIconClass(0);
+  }
+
+  get icon2(): string {
+    return this.getIconClass(1);
   }
 
   get hasLabel(): boolean {
     return this.label !== '';
+  }
+
+  private getIconClass(index: number) {
+    let line = `fa fa-${this.getValue(this.params.icon, index)}`;
+    if (this.getValue(this.params.extra, index) !== '') {
+      line += ` ${this.getValue(this.params.extra, index)}`;
+    }
+    if (this.stacked) {
+      let stackIndex = index === 0 ? 1 : 2;
+      line += ` fa-stack-${stackIndex}x`;
+    } else {
+      line += ' fa-fw fa-lg';
+    }
+    return line;
+  }
+
+  private getValue(obj: any, index: number) {
+    if (obj === null) {
+      return '';
+    }
+    if (this.stacked) {
+      return obj[index] || '';
+    } else {
+      return obj || '';
+    }
   }
 }
